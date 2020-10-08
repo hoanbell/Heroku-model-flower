8158b7bfc25aae06d95f2ba8e8b41d1b603d81b0
hoanbell <69511412+hoanbell@users.noreply.github.com>
2020-10-08 15:36:19 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,60 +1,15 @@
-import tensorflow as tf
+from tensorflow.python.keras.utils.data_utils import get_file
 from tensorflow.python.keras.preprocessing import image
 from tensorflow.python.keras.models import load_model
-from flask import Flask
-from flask import jsonify, request
-import numpy as np
-import heapq
-import cv2
+from tensorflow.python.keras.applications import ResNet50
+import matplotlib.pyplot as plt
+import os
 
-app = Flask(__name__)
-
-
-
-
-def topacc(imagepath):
-    model = load_model(r'model')
-
-    categories = ('alstroemeria', 'anemone', 'anthurium', 'arumlily', 'baloon flower', 'bellisdaisy', 'birdofparadise',
-              'bouvardia', 'cherryblossom', 'coneflower',
-              'cornflower', 'cypress', 'daffodil', 'dahlia', 'daisy', 'dandelion', 'dandelion',
-              'edelweiss flower', 'foxglove', 'gazania',
-              'hibicus', 'honeysuckle', 'hydrangea', 'iris', 'jasminum polyanthum', 'jasminum sambac', 'lantana',
-              'laurel', 'lilac', 'lilies',
-              'lilyofthevalley', 'lotus', 'loveinthemist', 'lupin', 'morningglory', 'myosotis', 'myrtus', 'orchid',
-              'pansy', 'plumeria',
-              'poinsettia', 'protea', 'ranunculus', 'rose', 'spearthistle', 'sunflower', 'tansy', 'tulip',
-              'waterlilies', 'whiteclover', 'yarrow')
-    x = np.expand_dims(imagepath, axis=0)
-    imagepath = cv2.cvtColor(imagepath, cv2.COLOR_BGR2RGB)
-    imagepath = cv2.resize(imagepath, (224, 224)).astype('float16')
-    preds = model.predict(np.expand_dims(imagepath, axis=0))
-    preds = np.array(preds).mean(axis=0)
-    return preds
-
-
-@app.route('/', methods=['GET', 'POST'])
-def uploadfile():
-    file = request.files['image'].read()
-    npimg = np.fromstring(file, np.uint8)
-    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-    try:
-        result = str(topacc(img))
-        return (result)
-    except:
-        print('failed')
-    return "DONE:DONE"
-
-
+def load_model()
+    model = ResNet50(weights='imagenet')
+    return("Loaded")
 
 
 @app.route('/hello')
 def hello_world():
-    return "<h1>Welcome to Summoner's Drift</h1>"
-
-
-
-
-@app.route('/hello')
-def hello_world():
-    return "<h1>Welcome to Summoner's Drift</h1>"
+    return(load_model)
