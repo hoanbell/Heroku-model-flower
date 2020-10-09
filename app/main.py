c07fc7bdc61eff40fa8eeade24427710db35ee65
hoanbell <69511412+hoanbell@users.noreply.github.com>
2020-10-09 13:46:28 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,35 +20,35 @@ categories = ['Alstroemeria', 'Anemone', 'Anthurium', 'Zantedeschia aethiopica',
               'Poinsettia', 'Protea', 'Ranunculus', 'Rose', 'Cirsium vulgare', 'Sunflower', 'Tansy', 'Tulip',
               'Nymphaeaceae', 'Clover', 'Yarrow']
 
-def topacc(num, imagepath):
-    # Num is the Number of the highest predicted, NOT larger than 5
-    # img = image.load_img(imagepath, target_size=(224, 224))
-    # x = image.img_to_array(imagepath)
-    x = np.expand_dims(imagepath, axis=0)
-    imagepath = cv2.cvtColor(imagepath, cv2.COLOR_BGR2RGB)
-    imagepath = cv2.resize(imagepath, (224, 224)).astype('float16')
-#     preds = model.predict(np.expand_dims(imagepath, axis=0))
-#     preds = np.array(preds).mean(axis=0)
-#     tops = sorted(preds, reverse=True)
-    origin_dict = {'result': {
-        'topacc1': {
-            'flowername': 'one',
-            'content': 'two',
-            'wikiurl': 'three'
-        }
-    }
-    }
-    return origin_dict
+# def topacc(num, imagepath):
+#     # Num is the Number of the highest predicted, NOT larger than 5
+#     # img = image.load_img(imagepath, target_size=(224, 224))
+#     # x = image.img_to_array(imagepath)
+#     x = np.expand_dims(imagepath, axis=0)
+#     imagepath = cv2.cvtColor(imagepath, cv2.COLOR_BGR2RGB)
+#     imagepath = cv2.resize(imagepath, (224, 224)).astype('float16')
+# #     preds = model.predict(np.expand_dims(imagepath, axis=0))
+# #     preds = np.array(preds).mean(axis=0)
+# #     tops = sorted(preds, reverse=True)
+#     origin_dict = {'result': {
+#         'topacc1': {
+#             'flowername': 'one',
+#             'content': 'two',
+#             'wikiurl': 'three'
+#         }
+#     }
+#     }
+#     return origin_dict
     
    
-app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
-def uploadfile():
-    file = request.files['image'].read()
-    npimg = np.fromstring(file, np.uint8)
-    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-    result = str(topacc(3, img))
-    return result
+# app = Flask(__name__)
+# @app.route('/', methods=['GET', 'POST'])
+# def uploadfile():
+#     file = request.files['image'].read()
+#     npimg = np.fromstring(file, np.uint8)
+#     img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+#     result = str(topacc(3, img))
+#     return result
   
 @app.route('/hello')
 def hello_world():
